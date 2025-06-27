b55302b06d5ad5ec7bd1e836a5acacbc22a29a38
Khalazarx <pipozyu@gmail.com>
2025-06-27 17:46:19 +0700

    update string escape

--- scripts/string.js
+++ scripts/string.js
@@ -5,3 +5,17 @@ document.writeln("<h3>Make a string with double quotes</h3>");
 document.writeln("Nama : John Wick" + "<br>" + "Umur : 30 Tahun" + "<br>" + "Pekerjaan : Pembunuh Bayaran" + "<br>");
 document.writeln("<br>");
 document.writeln("Nama : Adit Tua" + "<br>" + "Alamat : Jl. Merdeka No. 1" + "<br>" +"Telepon : 08123456789");
+
+
+// Escape Sequences
+// ** \n for Enter
+// ** \t for Tab
+// ** \' for Single Quote
+// ** \" for Double Quote
+// ** \\ for Backslash
+
+document.writeln("<h3>Escape Sequences</h3>");
+document.writeln("Ali \n adalah seorang programmer yang sangat cupu." + "<br>");
+document.writeln("Dia selalu menggunakan \"console.log\" untuk mencetak output." + "<br>");
+document.writeln("Dia juga suka menggunakan \\ untuk menampilkan karakter backslash." + "<br>");
+document.writeln("Dia sering menggunakan \' untuk menampilkan karakter petik tunggal." + "<br>");
